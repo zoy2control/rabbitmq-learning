221e315a824df3ba4225f4828bf0985907768d50
zoy2control <paulino_zoy_it@163.com>
2019-02-17 22:01:53 +0800

    rabbitmq init v0.1

---
+++ ch1-1/src/main/java/com/zoy/rabbitmq/zoyrabbitmqlearning/ZoyRabbitmqLearningApplication.java
@@ -0,0 +1,13 @@
+package com.zoy.rabbitmq.zoyrabbitmqlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoyRabbitmqLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoyRabbitmqLearningApplication.class, args);
+	}
+
+}
