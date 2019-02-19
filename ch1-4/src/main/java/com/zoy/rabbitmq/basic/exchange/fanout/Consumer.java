1280f1861fa83dbc2450c55ea7bc0c97f24f9ea8
zoy2control <paulino_zoy_it@163.com>
2019-02-19 23:47:36 +0800

    广播式交换机 exchange--fanout v0.1

---
+++ ch1-4/src/main/java/com/zoy/rabbitmq/basic/exchange/fanout/Consumer.java
@@ -0,0 +1,51 @@
+package com.zoy.rabbitmq.basic.exchange.fanout;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by zoypong on 2018/10/25.
+ */
+public class Consumer {
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("127.0.0.1");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+        // ·是否超时重连
+        connectionFactory.setAutomaticRecoveryEnabled(true);
+        connectionFactory.setNetworkRecoveryInterval(3000);
+
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+
+        String exchangeName = "test_fanout_exchange";
+        String exchangeType = "fanout";
+        String queueName = "test_topic_queue";
+        String routingKey = ""; // ·不指定 routingKey，只要 Exchange绑定 queue就可以
+//        String routingKey = "test.*"; // ·匹配刚好一个
+//        String routingKey = "test.#"; // ·匹配一个或多个
+
+        // ·声明 交换机和 队列，并将他们做 绑定
+        channel.exchangeDeclare(exchangeName, exchangeType, true, false, false, null);
+        channel.queueDeclare(queueName, false, false, false, null);
+        channel.queueBind(queueName, exchangeName, routingKey);
+
+        // ·声明 consumer
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+        // ·设置 channel
+        channel.basicConsume(queueName, true, consumer);
+
+        // ·获取消息
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            String recvMsg = new String(delivery.getBody());
+            System.out.println("消费者收到消息：" + recvMsg);
+        }
+    }
+}
