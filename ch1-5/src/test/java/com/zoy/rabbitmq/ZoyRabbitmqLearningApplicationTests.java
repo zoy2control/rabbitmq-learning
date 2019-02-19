6df055aeec1c7de212ed853d2e107df3073688c5
zoy2control <paulino_zoy_it@163.com>
2019-02-19 23:59:04 +0800

    消息体属性 message properties v0.1

---
+++ ch1-5/src/test/java/com/zoy/rabbitmq/ZoyRabbitmqLearningApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zoy.rabbitmq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZoyRabbitmqLearningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
