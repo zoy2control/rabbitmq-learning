599fcf5a51fc2af7fc5994e7c7e12d11b8d4a991
zoy2control <paulino_zoy_it@163.com>
2019-02-19 23:01:39 +0800

    quickstart v0.1

---
+++ ch1-1/src/main/java/com/zoy/rabbitmq/quickstart/Consumer.java
@@ -0,0 +1,42 @@
+package com.zoy.rabbitmq.quickstart;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.QueueingConsumer.Delivery;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by zoypong on 2018/10/21.
+ */
+public class Consumer {
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        // ·1、创建一个ConnectionFactory，并配置
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("127.0.0.1");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+        // ·2、通过 连接工厂创建 Connection
+        Connection connection = connectionFactory.newConnection();
+        // ·3、通过 Connection创建 Channel
+        Channel channel = connection.createChannel();
+
+        // ·4、声明一个队列
+        String queueName = "test001";
+        channel.queueDeclare(queueName, true, false, false,null);
+        // ·5、创建消费者（区别 producer，这里不是直接在 channel里操作）
+        QueueingConsumer queueingConsumer = new QueueingConsumer(channel);
+        // ·6、设置Channel
+        channel.basicConsume(queueName, true, queueingConsumer);
+
+        // ·7、获取消息
+        while (true) {
+            Delivery delivery = queueingConsumer.nextDelivery();
+            String msgOfGetting = new String(delivery.getBody());
+            System.out.println("消费端接受的消息：" + msgOfGetting);
+        }
+    }
+}
